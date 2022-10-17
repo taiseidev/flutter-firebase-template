ac08e653c2c7c087ffb60e782608c55a19bffbe3
onishi taisei <taisei.developer@gmail.com>
2022-10-17 20:39:00 +0900

    ユーザー作成時にカウントを追加するメソッドを作成

--- functions/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export namespace Constants {
-  const DEFAULT_COUNT = 0;
-}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,21 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
+const firestore = admin.firestore();
 
-export const createUser = functions
-  .region("asia-northeast1")
+const DEFAULT_REGION = "asia-northeast1";
+
+exports.createUser = functions
+  .region(DEFAULT_REGION)
   .firestore.document("users/{userId}")
-  .onCreate((snap) => {
-    const value = snap.data();
-    admin.auth().setCustomUserClaims(value.id, {
-      role: value.type == 0 ? "管理者" : "スタッフ",
-    });
+  .onCreate(async (snap) => {
+    const newValue = snap.data();
+    await firestore.collection("users").doc(newValue.id).set(
+      {
+        followCount: 0,
+        followerCount: 0,
+        myPostCount: 0,
+      },
+      { merge: true }
+    );
   });
