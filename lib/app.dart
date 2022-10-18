7ac02eda88a993c97712e64a8a879ff5af1c46a6
onishi taisei <taisei.developer@gmail.com>
2022-10-18 09:18:07 +0900

    GoRouterを追加

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_template/presentation/auth/profile/profile_page.dart';
 import 'package:flutter_firebase_template/presentation/auth/sns/sns_auth_page.dart';
+import 'package:go_router/go_router.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SnsAuthPage(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routeInformationProvider: _router.routeInformationProvider,
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
       debugShowCheckedModeBanner: false,
       title: 'Flutter-Firebase-Template',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SnsAuthPage(),
     );
   }
 }
