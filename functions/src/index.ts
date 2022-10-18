92446ac444575843f7fd6e75a486df7a6f8519c6
onishi taisei <taisei.developer@gmail.com>
2022-10-19 00:57:22 +0900

    リファクタ

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,7 +11,7 @@ const USERS_COLL = "users";
 exports.createUser = functions
   .region(DEFAULT_REGION)
   .firestore.document(`${USERS_COLL}/{userId}`)
-  .onCreate(async (snap) => {
+  .onCreate(async (snap): Promise<void> => {
     const newValue = snap.data();
     await firestore.collection(`${USERS_COLL}`).doc(newValue.id).set(
       {
@@ -26,7 +26,7 @@ exports.createUser = functions
 exports.incrementPostCount = functions
   .region(DEFAULT_REGION)
   .firestore.document(`${USERS_COLL}/{userId}/myPosts/{postId}`)
-  .onCreate(async (snap) => {
+  .onCreate(async (snap): Promise<void> => {
     const newValue = snap.data();
     await firestore
       .collection(`${USERS_COLL}`)
@@ -42,7 +42,7 @@ exports.incrementPostCount = functions
 exports.incrementFollowCount = functions
   .region(DEFAULT_REGION)
   .firestore.document(`${USERS_COLL}/{userId}/follows/{followId}`)
-  .onCreate(async (_, context) => {
+  .onCreate(async (_, context): Promise<void> => {
     const userId = context.auth?.uid;
     await firestore
       .collection(`${USERS_COLL}`)
@@ -58,7 +58,7 @@ exports.incrementFollowCount = functions
 exports.incrementFollowersCount = functions
   .region(DEFAULT_REGION)
   .firestore.document(`${USERS_COLL}/{userId}/followers/{followerId}`)
-  .onCreate(async (_, context) => {
+  .onCreate(async (_, context): Promise<void> => {
     const userId = context.auth?.uid;
     await firestore
       .collection(`${USERS_COLL}`)
@@ -69,4 +69,20 @@ exports.incrementFollowersCount = functions
         },
         { merge: true }
       );
+  });
+
+exports.incrementLikeCount = functions
+  .region(DEFAULT_REGION)
+  .firestore.document(`posts/{postId}/likeUsers/{likeUsersId}`)
+  .onCreate(async (_, context): Promise<void> => {
+    // postIdを取得してlikeCountsをインクリメントする必要がある。。
+    // await firestore
+    //   .collection(`posts`)
+    //   .doc(postId!)
+    //   .set(
+    //     {
+    //       likeCount: admin.firestore.FieldValue.increment(INCREMENT_COUNT),
+    //     },
+    //     { merge: true }
+    //   );
   });
