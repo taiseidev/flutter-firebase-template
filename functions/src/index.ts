9b13c99b353b6e49dc41fdbf56d1957b71dc7ff6
onishi taisei <taisei.developer@gmail.com>
2022-10-19 00:11:25 +0900

    投稿数を増加させるメソッドを追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,13 +5,15 @@ const firestore = admin.firestore();
 
 const DEFAULT_REGION = "asia-northeast1";
 const DEFAULT_COUNT = 0;
+const INCREMENT_COUNT = 1;
+const USERS_COLL = "users";
 
 exports.createUser = functions
   .region(DEFAULT_REGION)
-  .firestore.document("users/{userId}")
+  .firestore.document(`${USERS_COLL}/{userId}`)
   .onCreate(async (snap) => {
     const newValue = snap.data();
-    await firestore.collection("users").doc(newValue.id).set(
+    await firestore.collection(`${USERS_COLL}`).doc(newValue.id).set(
       {
         followCount: DEFAULT_COUNT,
         followerCount: DEFAULT_COUNT,
@@ -19,4 +21,20 @@ exports.createUser = functions
       },
       { merge: true }
     );
+  });
+
+exports.incrementPostCount = functions
+  .region(DEFAULT_REGION)
+  .firestore.document(`${USERS_COLL}/{userId}/myPosts/{postId}`)
+  .onCreate(async (snap) => {
+    const newValue = snap.data();
+    await firestore
+      .collection(`${USERS_COLL}`)
+      .doc(newValue.id)
+      .set(
+        {
+          myPostCount: admin.firestore.FieldValue.increment(INCREMENT_COUNT),
+        },
+        { merge: true }
+      );
   });
