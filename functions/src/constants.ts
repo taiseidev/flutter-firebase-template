1044e3763896ddb1936c419c9e56083a06b84f53
onishi taisei <taisei.developer@gmail.com>
2022-10-19 22:46:58 +0900

    constantsファイルを作成

---
+++ functions/src/constants.ts
@@ -0,0 +1,13 @@
+namespace Constants {
+  export const REGION_DEFAULT = "asia-northeast1";
+
+  export const TRIGGERS = "triggers";
+  export const USERS = "users";
+
+  export const ON_CREATE = "onCreate";
+  export const ON_UPDATE = "onUpdate";
+  export const ON_DELETE = "onDelete";
+
+  export const DEFAULT_COUNT = 0;
+  export const INCREMENT_COUNT = 1;
+}
