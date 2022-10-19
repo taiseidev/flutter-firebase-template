eec386ca5993675196142f5c6b45d709701c4813
onishi taisei <taisei.developer@gmail.com>
2022-10-19 22:53:59 +0900

    リファクタ

--- functions/src/triggers/onCreate/createUser.ts
+++ functions/src/triggers/onCreate/createUser.ts
@@ -4,11 +4,12 @@ const firestore = admin.firestore();
 
 export const trigger = async (snapshot: Snapshot, context: Context) => {
   const newValue = snapshot.data();
-  await firestore.collection("users").doc(newValue.id).set(
+  const count = Constants.DEFAULT_COUNT;
+  await firestore.collection(Constants.USERS).doc(newValue.id).set(
     {
-      followCount: 0,
-      followerCount: 0,
-      myPostCount: 0,
+      followCount: count,
+      followerCount: count,
+      myPostCount: count,
     },
     { merge: true }
   );
